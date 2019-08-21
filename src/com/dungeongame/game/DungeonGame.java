33d23f81182c9160daa23b744eb46d233724ef68
Jackie-Tran <jtran2009a@gmail.com>
2019-08-21 16:48:16 -0400

    Created the game class

--- src/com/dungeongame/game/DungeonGame.java
+++ src/com/dungeongame/game/DungeonGame.java
@@ -1,5 +1,32 @@
 package com.dungeongame.game;
 
-public class DungeonGame {
+import com.mikejack.engine.AbstractGame;
+import com.mikejack.engine.GameContainer;
+import com.mikejack.engine.Screen;
+
+public class DungeonGame extends AbstractGame{
+
+    public static final int WIDTH=320, HEIGHT=180, SCALE=4;
+    public static final String TITLE = "Dungeon Game";
+    
+    public DungeonGame() {
+	
+    }
+
+    @Override
+    public void update(GameContainer gc, float dt) {
+	
+    }
+    
+    @Override
+    public void render(GameContainer gc, Screen screen) {
+	
+    }
+    
+    public static void main(String args[]) {
+	GameContainer gc = new GameContainer(WIDTH, HEIGHT, SCALE, TITLE, new DungeonGame());
+	gc.setFpsVisibility(true);
+	gc.start();
+    }
 
 }
